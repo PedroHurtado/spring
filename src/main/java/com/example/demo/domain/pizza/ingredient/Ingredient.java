7d017a79fa41573863b3e74b2decdaedf5336b50
Pedro Hurtado <pedrohurt@gmail.com>
2023-06-12 13:44:35 +0000

    add ingredien

---
+++ src/main/java/com/example/demo/domain/pizza/ingredient/Ingredient.java
@@ -0,0 +1,47 @@
+package com.example.demo.domain.pizza.ingredient;
+
+import java.util.UUID;
+
+
+
+import lombok.Getter;
+import lombok.Value;
+
+@Getter
+public class Ingredient {
+
+    private final Ingredient.IngredientId id;
+    private  String name;
+    private  double price;
+    protected Ingredient(IngredientId id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void Update(String name,double price){
+        //UPDATE EVENT
+        this.name = name;
+        this.price = price;
+    }
+    public static Ingredient Create(IngredientId id, String name, double price){
+        //CREATE EVENT 
+        return new Ingredient(id, name, price);
+    }
+    @Override
+    public boolean equals(Object o) {
+        
+       if(o instanceof IngredientId i){
+        return i.value.equals(id.value);
+       }
+       return false;
+    }
+    @Override
+    public int hashCode(){
+        return id.value.hashCode();
+    }
+    @Value
+    public static class IngredientId{
+        private final UUID value;
+    }
+}
