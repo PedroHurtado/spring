c37d3d325c3a19592729ce504b277feb6de86ae6
Pedro Hurtado <pedrohurt@gmail.com>
2023-06-12 16:24:38 +0200

    add pizza

---
+++ src/main/java/com/example/demo/domain/pizza/IngredientPizza.java
@@ -0,0 +1,45 @@
+package com.example.demo.domain.pizza;
+
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Value;
+
+@Getter
+public class IngredientPizza {
+    
+    private final IngredientPizzaId id;
+    private  String name;
+    private  double price;
+    protected IngredientPizza(IngredientPizzaId id, String name, double price){
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void Update(String name,double price){
+        //UPDATE EVENT
+        this.name = name;
+        this.price = price;
+    }
+    public static IngredientPizza Create(IngredientPizzaId id, String name, double price){
+        //CREATE EVENT 
+        return new IngredientPizza(id, name, price);
+    }
+    @Override
+    public boolean equals(Object o) {
+        
+       if(o instanceof IngredientPizzaId i){
+        return i.equals(id);
+       }
+       return false;
+    }
+    @Override
+    public int hashCode(){        
+        return id.hashCode();
+    }
+    @Value
+    public static class IngredientPizzaId{
+        private final UUID value;
+    }
+}
--- src/main/java/com/example/demo/domain/pizza/Pizza.java
+++ src/main/java/com/example/demo/domain/pizza/Pizza.java
@@ -1,5 +1,69 @@
 package com.example.demo.domain.pizza;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import lombok.Getter;
+import lombok.Value;
+
+@Getter
 public class Pizza {
-    
+    private final PizzaId id;
+    private String name;
+    private String description;
+    private String url;
+    private Set<IngredientPizza> ingredients = new HashSet<>();
+
+    public Pizza(PizzaId id, String name, String description, String url) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    public void addIngredient(IngredientPizza ingredient) {
+        this.ingredients.add(ingredient);
+    }
+
+    public void removeIngredient(IngredientPizza ingredient) {
+        if (this.ingredients.contains(ingredient)) {
+            this.ingredients.remove(ingredient);
+        }
+    }
+
+    public double getPrice() {
+        return this.ingredients.stream()
+                .map(i -> i.getPrice())
+                .reduce(0.0D, (acumulator, value) -> acumulator + value) * 1.2D;
+        
+    }
+
+    public List<IngredientPizza> getIngredients(){
+        return ingredients.stream().toList();
+    }
+
+    public static Pizza create(PizzaId id, String name, String description, String url) {
+
+        return new Pizza(id, name, description, url);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof PizzaId p) {
+            return p.equals(id);
+        }
+        return false;
+    }
+
+    @Value
+    public static class PizzaId {
+        private final UUID value;
+    }
 }
